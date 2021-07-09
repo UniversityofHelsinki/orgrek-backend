a743a67cdd8ea2ae6d10a005c4f909d47c58187f
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-07-09 10:29:07 +0300

    use openshift ipaddress and port if running in openshift

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, ipaddress, () => {
+    console.log( "Listening on " + ipaddress + ", port " + port );
 })
