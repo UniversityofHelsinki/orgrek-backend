c1153d62f4540c36ec4aa42f0e85acab3c746ad1
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-07-12 09:40:34 +0300

    add cors package and /api endpoint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
 
 app.listen(port, ipaddress, () => {
     console.log( "Listening on " + ipaddress + ", port " + port );
-})
+});
