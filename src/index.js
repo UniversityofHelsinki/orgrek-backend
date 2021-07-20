d2b1733e1509144e85c3ed9fe565de9896bf2607
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-07-20 15:14:10 +0300

    testing for making a direct connection to api which is behind shibboleth

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fetch = require('node-fetch');
+const https = require('https');
 const ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+});
 
 app.use(cors());
 
@@ -14,7 +19,7 @@ app.get('/', (req, res) => {
 app.get("/api/hello", async (req, res) => {
     try {
         console.log(req.headers);
-        const response = await fetch('https://organisaatiorekisteri.ohtu-dev.it.helsinki.fi/api/hierarchy/a1/2021-07-20/justrootnode', {method: 'GET', headers: req.headers}  );
+        const response = await fetch('https://organisaatiorekisteri.ohtu-dev.it.helsinki.fi/api/hierarchy/a1/2021-07-20/justrootnode', {method: 'GET', headers: req.headers, agent: httpsAgent}  );
         const data = await response.json();
         console.log(data);
     } catch (err) {
