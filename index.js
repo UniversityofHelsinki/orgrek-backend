5d16cf13cb911da3c76a3c19d1f96185e4e0dcf3
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-07-23 12:29:30 +0300

    added bodyParser and cookieParser packages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const compression = require('compression');
 const cors = require('cors');
@@ -13,6 +15,10 @@ const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.use(cors());
 app.use(compression());
 app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const router = express.Router();
 app.use('/api', router);
