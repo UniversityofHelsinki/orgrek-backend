47ade104eb5a54e46aebb9e46927371f2f45e034
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-09-14 10:37:06 +0300

    trying to get hello message from old backend

--- api/api-backend.js
+++ api/api-backend.js
@@ -5,9 +5,12 @@ const apiBackendHost = process.env.API_BACKEND_HOST;
 exports.hello = async (req, res) => {
     try {
         console.log(req.headers);
+        let headers = {
+            'accept': 'application/json',
+        }
         const response = await fetch(`${apiBackendHost}/api/user`, {
             method: 'GET',
-            headers: req.headers
+            headers: headers
         });
         const data = await response.json();
         console.log(data);
