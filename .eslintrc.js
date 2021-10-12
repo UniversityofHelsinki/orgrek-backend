e68350beca293f43019e24706d0719db2ca902ce
sinkkila <reetta.sinkkila@helsinki.fi>
2021-10-12 14:40:27 +0300

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
     },
-    "parserOptions": {
-        "ecmaVersion": 2018
+    'parserOptions': {
+        'ecmaVersion': 2018
     },
-    "rules": {
-        "indent": [2, 4],
-        "linebreak-style": [
-            "error",
-            "unix"
+    'rules': {
+        'indent': [2, 4],
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "warn",
-            "single"
+        'quotes': [
+            'warn',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ],
     }
 };
