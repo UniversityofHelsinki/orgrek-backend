8739323d986619aba0564f10a14cbf6a6ba1c1b9
Henriikka Palva <henriikka.palva@helsinki.fi>
2022-04-19 17:37:39 +0300

    typo fix

--- Constants.js
+++ Constants.js
@@ -3,9 +3,9 @@ const SHIBBOLETH_COOKIE_NAME = '_shibsession_';
 const ROLE_READER = 'ROLE_READER';
 
 const MAPPED_ROLES = Object.freeze({
-    ROLE_WRITER: 'grp-orgkrek-role-writer',
-    ROLE_ADMIN: 'grp-orgkrek-role-admin',
-    ROLE_READER_ALL: 'grp-orgkrek-role-reader-all'
+    ROLE_WRITER: 'grp-orgrek-role-writer',
+    ROLE_ADMIN: 'grp-orgrek-role-admin',
+    ROLE_READER_ALL: 'grp-orgrek-role-reader-all',
 });
 
 module.exports = {
