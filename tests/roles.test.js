4a608fe51c8abb5c5287d08fb1cbf31756c3cbfb
Henriikka Palva <henriikka.palva@helsinki.fi>
2022-04-19 17:40:12 +0300

    typo fix

--- tests/roles.test.js
+++ tests/roles.test.js
@@ -8,22 +8,22 @@ describe('Role tests based on request headers', () => {
       expect(calculateUserRoles(user).roles).toEqual(  ["ROLE_READER"]);
    });
 
-   it('adds ROLE_ADMIN from iam-group grp-orgkrek-role-admin',  () => {
-      const user = { eppn : 'test', preferredlanguage: 'fi', hyGroupCn: 'grp-orgkrek-role-admin'}
+   it('adds ROLE_ADMIN from iam-group grp-orgrek-role-admin',  () => {
+      const user = { eppn : 'test', preferredlanguage: 'fi', hyGroupCn: 'grp-orgrek-role-admin'}
       expect(calculateUserRoles(user).roles).toBeDefined();
       expect(calculateUserRoles(user).roles.length).toEqual(2);
       expect(calculateUserRoles(user).roles).toEqual(  ["ROLE_READER", "ROLE_ADMIN"]);
    });
 
-   it('adds ROLE_WRITER from iam-group grp-orgkrek-role-admin',  () => {
-      const user = { eppn : 'test', preferredlanguage: 'fi', hyGroupCn: 'grp-orgkrek-role-writer'}
+   it('adds ROLE_WRITER from iam-group grp-orgrek-role-admin',  () => {
+      const user = { eppn : 'test', preferredlanguage: 'fi', hyGroupCn: 'grp-orgrek-role-writer'}
       expect(calculateUserRoles(user).roles).toBeDefined();
       expect(calculateUserRoles(user).roles.length).toEqual(2);
       expect(calculateUserRoles(user).roles).toEqual(  ["ROLE_READER", "ROLE_WRITER"]);
    });
 
-   it('adds ROLE_READER_ALL from iam-group grp-orgkrek-role-reader-all',  () => {
-      const user = { eppn : 'test', preferredlanguage: 'fi', hyGroupCn: 'grp-orgkrek-role-reader-all'}
+   it('adds ROLE_READER_ALL from iam-group grp-orgrek-role-reader-all',  () => {
+      const user = { eppn : 'test', preferredlanguage: 'fi', hyGroupCn: 'grp-orgrek-role-reader-all'}
       expect(calculateUserRoles(user).roles).toBeDefined();
       expect(calculateUserRoles(user).roles.length).toEqual(2);
       expect(calculateUserRoles(user).roles).toEqual(  ["ROLE_READER", "ROLE_READER_ALL"]);
