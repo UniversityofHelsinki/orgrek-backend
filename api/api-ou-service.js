0a1d596503c3f7a4a58229f42c10d2c2e55247fd
anttilan <antti.lankinen@helsinki.fi>
2023-07-04 07:38:54 +0300

    Alayksikön päivitys

--- api/api-ou-service.js
+++ api/api-ou-service.js
@@ -291,6 +291,23 @@ exports.updateParents = async (req, res) => {
     }
 };
 
+exports.updateChildren = async (req, res) => {
+    try {
+        const url = `${apiOuServiceHost}/api/edge/children`;
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(req.body)
+        });
+        return res.status(response.status).json(await response.json());
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+};
+
 exports.updateNodeNameAttributes = async (req, res) => {
     try {
         const url = `${apiOuServiceHost}/api/node/${req.params.id}/attributes/names`;
